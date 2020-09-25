18302bc3a5cf4c65164ecdeaf47e57d995bdbbfa
bhargavmln <bhargavmln@gmail.com>
2020-09-25 21:23:03 +0530

    Starting at potision 0

--- snakeAndLadder.java
+++ snakeAndLadder.java
@@ -1,6 +1,6 @@
 public class snakeAndLadder{
 	public static void main(String[] args) {
-		int postion = 0;
+		int position = 0;
 		System.out.println("Your current position is :" + position);
 	}
 }
