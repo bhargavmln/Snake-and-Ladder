ef63cd8c3e3a89154efb669ff79b52df7deff0fc
bhargavmln <bhargavmln@gmail.com>
2020-09-26 16:01:45 +0530

    UC2 added

--- snakeAndLadder.java
+++ snakeAndLadder.java
@@ -1,6 +1,11 @@
 public class snakeAndLadder{
 	public static void main(String[] args) {
+		
 		int position = 0;
-		System.out.println("Your current position is :" + position);
+		System.out.println("Your current position is: " + position);
+		
+		int dieNumber = ((int) Math.floor(Math.random()*10) % 6) + 1;
+		System.out.println("The number rolled on die is " + dieNumber);
+		
 	}
 }
