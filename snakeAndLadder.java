973b5a275a272e2cba5a049f3417a69b8bc9d3a6
bhargavmln <bhargavmln@gmail.com>
2020-09-26 16:59:37 +0530

    added UC4

--- snakeAndLadder.java
+++ snakeAndLadder.java
@@ -8,23 +8,23 @@ public class snakeAndLadder{
 		int position = 0;
 		System.out.println("Your start position is: " + position);
 		
-		int dieNumber = ((int) Math.floor(Math.random()*10) % 6) + 1;
-		System.out.println("The number rolled on die is " + dieNumber);
-
 		
-		int playOption = ((int) Math.floor(Math.random()*10) % 2);
-		switch(playOption) {
-		case snake:
-			position = position - dieNumber;
-			if(position<0)
-				position = 0;
-			break;
-		case ladder:
-			position = position + dieNumber;
-			break;
-		default :
-			position = position + 0;
-				
+		while(position<=100) {
+			int dieNumber = ((int) Math.floor(Math.random()*10) % 6) + 1;
+			int playOption = ((int) Math.floor(Math.random()*10) % 2);
+			
+			switch(playOption) {
+			case snake:
+				position = position - dieNumber;
+				if(position<0)
+					position = 0;
+				break;
+			case ladder:
+				position = position + dieNumber;
+				break;
+			default :
+				position = position + 0;
+			}		
 		}
 		System.out.println("Your Current position is: " + position);
 	}
