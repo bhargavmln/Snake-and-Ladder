a35991bcb6cabfc7cfccbe08480a2295121716b3
bhargavmln <bhargavmln@gmail.com>
2020-09-26 17:24:03 +0530

    UC5 added

--- snakeAndLadder.java
+++ snakeAndLadder.java
@@ -9,7 +9,7 @@ public class snakeAndLadder{
 		System.out.println("Your start position is: " + position);
 		
 		
-		while(position<=100) {
+		while(position<100) {
 			int dieNumber = ((int) Math.floor(Math.random()*10) % 6) + 1;
 			int playOption = ((int) Math.floor(Math.random()*10) % 2);
 			
@@ -21,11 +21,13 @@ public class snakeAndLadder{
 				break;
 			case ladder:
 				position = position + dieNumber;
+				if(position>100)
+					position = position - dieNumber;
 				break;
 			default :
 				position = position + 0;
-			}		
+			}
 		}
-		System.out.println("Your Current position is: " + position);
+		System.out.println("Congragulations! You have reached the position " + position);
 	}
 }
